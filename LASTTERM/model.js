f7d662fa3dd6c8c29e78420509031b910ef40f42
mauriceaaa <9400367@gmail.com>
2020-01-10 11:05:41 +0800

    Update model.js

--- LASTTERM/model.js
+++ LASTTERM/model.js
@@ -1,11 +1,34 @@
 const M = module.exports = {}
 
 const posts = []
+const db = {
+  profile: {
+    snoopy: {password:'123'}
+  },
+  users:{
+    snoopy: {
+      posts: [
+        {id: 0, title: '', body: ''},
+        {id: 1, title: '', body: ''},
+      ]
+    }
+  }
+  
+}
 
 M.add = function (post) {
   const id = posts.push(post) - 1
   post.created_at = new Date()
   post.id = id
+}
+M.login = function (user, password) {
+  const profile = db.profile[user]
+  console.log('profile=', profile, 'password=', password)
+  return (profile.password === password)
+}
+
+M.signup = function (user) {
+  return db.profile[user] == null
 }
 
 M.get = function (id) {
